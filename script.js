eb3c1f0e5d6c551dbbd63b10dae9e7c67af7bd70
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-13 12:10:09 -0400

    create init funciton to start game and reset game

--- script.js
+++ script.js
@@ -13,15 +13,29 @@ const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
 // Starting Conditions
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.hidden = true
+let scores, currentScore, activePlayer, playing
 
-const scores = [0, 0]
-let currentScore = 0 
-let activePlayer = 0
-let playing = true
+// Init
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0; 
+  activePlayer = 0;
+  playing = true;
 
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+  diceEl.hidden = true
+
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
+
+init()
 // fucntions
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -71,3 +85,5 @@ btnHold.addEventListener('click', function () {
     // switch to the next player
   }
 })
+
+btnNew.addEventListener('click', init)
